fb623d7df40864324d920a743c8820e84a1ac5c8
AgnSun <agncielinska@gmail.com>
2023-05-04 09:21:46 +0200

    added icons, deleting users from list, dialog

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -3,10 +3,12 @@ import loggedUserReducer from "./loggedUserReducer"
 import { userDatabaseReducer } from "./usersDatabaseReducer"
 import { chatsDatabaseReducer } from "./chatsDatabaseReducer"
 import { currentChatReducer } from "./currentChatReducer"
+import { deleteUserReducer } from "./deleteUserReducer"
 
 export const rootReducer = combineReducers({
   loggedUserReducer,
   userDatabaseReducer,
   chatsDatabaseReducer,
   currentChatReducer,
+  deleteUserReducer,
 })
