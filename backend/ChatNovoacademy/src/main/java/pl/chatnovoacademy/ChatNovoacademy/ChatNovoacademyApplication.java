1d10cd993b89c1ed0f3c30dfe744fdfd15b3eb87
Ml0dy <JakubWosiak99@gmail.com>
2023-06-07 13:09:46 +0200

    Added backend structure v2

---
+++ backend/ChatNovoacademy/src/main/java/pl/chatnovoacademy/ChatNovoacademy/ChatNovoacademyApplication.java
@@ -0,0 +1,13 @@
+package pl.chatnovoacademy.ChatNovoacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatNovoacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatNovoacademyApplication.class, args);
+	}
+
+}
---
+++ backend/ChatNovoacademy/src/test/java/pl/chatnovoacademy/ChatNovoacademy/ChatNovoacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.chatnovoacademy.ChatNovoacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatNovoacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
